ada0e599aa283cab105f24f3d40ff07967342773
William Hurst <will.hurstjr@gmail.com>
2019-07-31 21:23:56 -0700

    new files added

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+//When page first loads we are on welcome screen, text will fade in saying welcome and describing what the app does, what we're about. 
+//There will also be a name input field(optional). There will also be a start button. On the button press of the start button-
+
+//screen clears and new screen loading location input with greeting containing input name, checkboxes for categories listed and selection options listed also appear. Continue button appears 
+
+//On click of continue button, screen clears and loading icon appears with message through duration of ajax call search. 
+
+//After loading, a screen appears saying "Your Category for today is (result)", accept, decline buttons appear.
+
+//If decline button clicked, randomly pick the categories left, randomly search again, if category result matches last random selection
+
+//If accept button clicked, move to picking random restaurant in category.
+
+//When random restaurant is loaded up, accept and decline buttons pop up underneath
+
+//If random restaurant is accepted, on button press, all restaurant information appears including Google maps.
+
+//If random restaurant is declined, on button press, another random restaurant appears on search in category selected.
+
